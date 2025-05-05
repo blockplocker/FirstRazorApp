c66f04fa262fccb2f30dbd1abdb7c00da9abe44c
blockplocker <noahallklint@gmail.com>
2025-05-05 13:57:49 +0200

    Add Bootstrap page and navigation link
    - Introduced a new navigation link for "Bootstrap" in `_Layout.cshtml`.
    - Created `Bootstrap.cshtml` with Bootstrap components and layout.

---
+++ FirstRazorApp/Pages/Bootstrap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstRazorApp.Pages
+{
+    public class BootstrapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
